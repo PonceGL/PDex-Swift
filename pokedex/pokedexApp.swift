1fe7bc1ca1ffde60e5fa93519a59b1ece39083b5
PonceGL <poncianogl@hotmail.com>
2024-07-01 12:32:06 -0600

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Ponciano Guevara Lozano on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
